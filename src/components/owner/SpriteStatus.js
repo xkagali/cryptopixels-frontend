95ac1be6bd7185a6e06d11898aa9b10bc6827672
cwlronald <cwl.ronald@gmail.com>
2021-06-24 13:12:40 +0800

    fixed the pixel detail page

--- src/components/owner/SpriteStatus.js
+++ src/components/owner/SpriteStatus.js
@@ -1,31 +1,33 @@
 import React from 'react';
 
 function SpriteStatus({item, user}) {
-    // console.log(item)
-    // console.log(user)
-    // console.log(item.currentOwner?._id)
-    // console.log(user._id)
     return (
         <>
-            <div className="spriteInfo d-flex">
-                <div className="boughtDate">
-                    Purchased On:<br className="showDesktop"/>{item.datePurchased}
+            {item.currentOwner?._id == user?._id ?
+            <>
+                {item.inMarketplace ?
+                <div className="spriteInfo d-flex">
+                    <div className="spriteStatus list">
+                        You listed this Pixel
+                    </div>
+                    <div className="boughtDate">
+                        Listed On:<br className="showDesktop"/>{item.dateListed}
+                    </div>
                 </div>
-                <div className="boughtDate">
-                    Listed On:<br className="showDesktop"/>{item.dateListed}
-                </div>
-            </div>
-            {user && <div className="spriteInfo d-flex">
-                {item.currentOwner?._id == user?._id ?
+                :
+                <div className="spriteInfo d-flex">
                     <div className="spriteStatus own">
                         You own this Pixel
                     </div>
-                    :
-                    <div className="spriteStatus dontown">
-                        You do not own this Pixel
+                    <div className="boughtDate">
+                        Purchased On:<br className="showDesktop"/>{item.datePurchased}
                     </div>
+                </div>
                 }
-            </div>}
+            </>
+            :
+            <></>
+            }
         </>
     );
 }
